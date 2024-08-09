ae90ca1af3f3e42a689a8d88735a63994913faf2
むずでょ <muzudho1@gmail.com>
2024-08-09 10:34:45 +0900

    WIP Chapter 11

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
   function handleSearch(term: string) {
     console.log(`[handleSearch] ${term}`);
@@ -19,6 +21,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
     } else {
       params.delete('query');
     }
+
+    // replace(...) 関数はURLを置き換える。ユーザーが Lee と入力すると、例えば以下のコードの場合 `/dashboard/invoices?query=lee` といった感じになる
+    // `${pathname}` は現在のパス。この場合 "/dashboard/invoices"
+    // `params.toString()` は、URLの引数に使える書式になるのか？
+    replace(`${pathname}?${params.toString()}`);
   }
 
   return (
