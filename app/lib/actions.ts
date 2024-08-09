f910cef795cae24073f570c6f310803827401f3e
むずでょ <muzudho1@gmail.com>
2024-08-09 18:45:05 +0900

    WIP Chapter 12

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,6 +15,9 @@ const FormSchema = z.object({
  
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
@@ -43,3 +46,23 @@ export async function createInvoice(formData: FormData) {
     // console.log(rawFormData);
     // console.log(`amount の型：${typeof rawFormData.amount}`);
 }
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
+  
