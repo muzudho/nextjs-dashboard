a4cf30d43b82c7d5194f2bc8ccfd097ef519cc5f
むずでょ <muzudho1@gmail.com>
2024-08-10 14:39:15 +0900

    Chapter16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+// Next.JS が HTML の <head> の中の <meta> 要素を自動で作ってくれる
+export const metadata: Metadata = {
+  title: {
+    // %s はページのタイトルに置き換えられる
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
